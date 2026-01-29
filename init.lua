9e8e017722868b024d79be3576352716197ac898
zhaoxin <xin.zhao@datatist.com>
2026-01-29 18:53:10 +0800

    zhaoxin-mac-mini-m2.local.2026-01-29 18:53

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
 -- 创建日志记录器
-local log = hs.logger.new('main', 'debug')
+local log = hs.logger.new("main", "debug")
 hs.logger.historySize(1000)
 log.i("Hammerspoon 启动")
 
@@ -8,7 +8,7 @@ hs.ipc.cliInstall("/opt/homebrew/")
 package.path = package.path .. ";~/.hammerspoon/modules/?.lua"
 
 -- 切换应用切换到小写，锁屏关闭应用
-require('modules.appWatcher')
+require("modules.appWatcher")
 
 -- spoon doc
 --https://github.com/Hammerspoon/hammerspoon/blob/master/SPOONS.md
@@ -22,9 +22,9 @@ hs.loadSpoon("SpoonInstall")
 -- 使用字典或者notepad打开选中的单词
 spoon.SpoonInstall:andUse("LookupSelection")
 spoon.LookupSelection:bindHotkeys({
-    lexicon = { { "ctrl", "alt", "cmd" }, "l" },
-    neue_notiz = { { "ctrl", "alt", "cmd" }, "n" },
-    hsdocs = { { "ctrl", "alt", "cmd" }, "h" }
+	lexicon = { { "ctrl", "alt", "cmd" }, "l" },
+	neue_notiz = { { "ctrl", "alt", "cmd" }, "n" },
+	hsdocs = { { "ctrl", "alt", "cmd" }, "h" },
 })
 
 -- -- hot-key的modal管理方法
@@ -41,7 +41,18 @@ hs.alert("配置已重新加载")
 
 -- hs.eventtap.keyStroke({"cmd", "ctrl" }, "q")
 -- hs.eventtap.keyStroke({ "ctrl" }, "tab", 0)
-hs.loadSpoon('EmmyLua')
+hs.loadSpoon("EmmyLua")
 
 -- 调用这个加载location模块到定位隐私列表，进而可以打开权限
 hs.location.get()
+
+hs.hotkey.bind({}, "F15", function()
+	hs.application.launchOrFocus("wezterm")
+end)
+hs.hotkey.bind({}, "F16", function()
+	hs.application.launchOrFocus("obsidian")
+end)
+
+hs.hotkey.bind({}, "F17", function()
+	hs.application.launchOrFocus("Google Chrome")
+end)
