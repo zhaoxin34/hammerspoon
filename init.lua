290c58faca28c0a31b144303d5b539acff40d96c
zhaoxin34 <zhaoxin_er@hotmail.com>
2026-08-04 09:47:29 +0800

    Mac.2026-08-04 09:47

--- init.lua
+++ init.lua
@@ -56,7 +56,7 @@ hs.hotkey.bind({}, "F16", function()
 end)
 
 hs.hotkey.bind({}, "F17", function()
-	hs.application.launchOrFocus("Google Chrome")
+	hs.application.launchOrFocus("ego lite")
 end)
 
 -- spoon.SpoonInstall:andUse("ReloadConfiguration")
